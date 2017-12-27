260fbcd53c1b8ca256a42bc1fed09a09db85c098
fkice <ice@freshket.co>
2017-12-27 17:27:30 +0700

    update

--- App.js
+++ App.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
+      <Image source={require('./assets/images/Logo_FK.png')} style={{ flex: 1,width: 240,height: 80,resizeMode: 'contain'}}/>
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+
     );
   }
 }
